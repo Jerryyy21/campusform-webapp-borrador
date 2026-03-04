9f608e3de9b8b02c1608fca6bd105eb2cbc94a7e
Alejandro Perez <67518097+alexpc555@users.noreply.github.com>
2026-03-04 00:03:56 -0600

    creacion de carpetas para las vistas

---
+++ src/app/screens/panel-admin-profesor/categorias-administracion/categorias-administracion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-administracion',
+  imports: [],
+  templateUrl: './categorias-administracion.html',
+  styleUrl: './categorias-administracion.scss',
+})
+export class CategoriasAdministracion {
+
+}
---
+++ src/app/screens/panel-admin-profesor/reportes-admin/reportes-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reportes-admin',
+  imports: [],
+  templateUrl: './reportes-admin.html',
+  styleUrl: './reportes-admin.scss',
+})
+export class ReportesAdmin {
+
+}
