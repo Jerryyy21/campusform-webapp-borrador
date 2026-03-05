82081ebb38dc6bbe9f0eabfa75148adf68de8b16
Jerryyy21 <111446905+Jerryyy21@users.noreply.github.com>
2026-03-05 14:37:10 -0600

    modal nueva publicacion en mis-posts

--- src/app/screens/dashboard-secciones/mis-posts/mis-posts.ts
+++ src/app/screens/dashboard-secciones/mis-posts/mis-posts.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { SHARED_IMPORTS } from '../../../shared/shared.imports';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+
+import { NewPostModal } from '../../../partials/new-post-modal/new-post-modal';
 
 @Component({
   selector: 'app-mis-posts',
@@ -13,8 +16,16 @@ export class  MisPostsComponent {
    searchQuery = '';
   sortBy: 'recent' | 'popular' | 'comments' = 'recent';
 
-  constructor(private router: Router) {}
+  constructor(private dialog: MatDialog, private router: Router) {}
 
+  openNewPostModal() {
+    this.dialog.open(NewPostModal, {
+      width: '720px',
+      maxWidth: '92vw',
+      panelClass: 'cf-dialog',
+      autoFocus: false,
+    });
+  }
   goBackDashboard() {
     this.router.navigate(['/dashboard']);
   }
